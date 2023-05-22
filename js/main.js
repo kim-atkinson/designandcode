54dfd21a83e9ab33b4bce9bb00c9a53aef7bc31f
kim-atkinson <kimfromtoronto@gmail.com>
2023-05-22 10:21:59 -0400

    Fixed header to the top of the page.

--- js/main.js
+++ js/main.js
@@ -4,3 +4,26 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+// When user scrolls down the page, at a certain point
+// Add a class to make header white
+
+const headerTag = doucment.querySelector("header");
+
+const toggleHeader =  function () {
+  const pixels = window.pageYOffset 
+
+  if (pixels > 60) {
+    headerTag.classList.add("scrolled")
+  } else {
+    headerTag.classList.remove("scrolled");
+  }
+}
+
+
+toggleHeader();
+
+
+document.addEventListener("scroll", function () {
+  toggleHeader()
+})
